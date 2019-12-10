d81d20f339a5425e91e1ec27d514aa031b547cf2
Adam K. Purdy <admin@kpapro.com>
2019-12-09 19:15:04 -0500

    chore(upgrade)
    lint
    
    lint
    
    lint

--- base/src/boot/serverConnection.js
+++ base/src/boot/serverConnection.js
@@ -2,17 +2,11 @@ import * as base from '../services/firebase/base.js'
 
 const firebaseService = Object.assign({}, base)
 
-// "async" is optional
 export default async () => {
   const config = process.env.environments.FIREBASE_CONFIG
-  try {
-    await firebaseService.fBInit(config)
-    // Just a validation that our service structure is working
-    // This does not guartee that the initialization happened
-    // with a valid api key. This will be done when the app
-    // performs any auth() functionality
-    console.log('Firebase init called properly')
-  } catch (err) {
-    throw Error(`Error in firebase initilization: ${err}`)
-  }
+  firebaseService.fBInit(config)
+
+  // Validation that our service structure is working
+  // with a valid api key.
+  console.log(firebaseService.auth(), 'Firebase has been initialized with a valid key')
 }
--- base/src/services/firebase/base.js
+++ base/src/services/firebase/base.js
@@ -1,6 +1,16 @@
 import firebase from 'firebase/app'
+import 'firebase/auth'
 
-/** Convienience method to initialize firebase app
+/**
+ * Gets Firebase's auth service
+ * https://firebase.google.com/docs/reference/js/firebase.auth.html#callable
+ * @return {Interface} The Firebase Auth service interface
+ */
+export const auth = () => {
+  return firebase.auth()
+}
+
+/** Convenience method to initialize firebase app
  *
  * @param  {Object} config
  * @return {Object} App
