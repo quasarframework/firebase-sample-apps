d4d208b4a39392fdad5893cac89ba4e7516091ca
lw <leon.wolf@profirepro.de>
2020-02-23 23:58:18 +0100

    fixed small bug in UserSettings.vue that uploaded the wrong image (profile instead of background)

--- data/profile/src/pages/user/profile/UserSettings.vue
+++ data/profile/src/pages/user/profile/UserSettings.vue
@@ -1,145 +1,145 @@
 <template>
   <div class="user-settings full-width" v-if="currentUser">
     <q-form class="full-height" @submit="saveUserData">
-        <div class="background-photo">
-            <div class="default-background" v-if="showBackgroundPhoto()">
-                <q-img src="https://cdn.quasar.dev/img/material.png" style="height: 200px;" @click="showPhotoUpload('background')"></q-img>
-            </div>
-            <div class="user-background" v-else>
-                <q-img :src="currentUser.backgroundPhoto" style="height: 200px;" @click="showPhotoUpload('background')"></q-img>
-            </div>
+      <div class="background-photo">
+        <div class="default-background" v-if="showBackgroundPhoto()">
+          <q-img src="https://cdn.quasar.dev/img/material.png" style="height: 200px;" @click="showPhotoUpload('background')"></q-img>
         </div>
-        <div class="absolute-top q-mt-sm text-white text-center">
-            <q-icon class="q-mr-sm" color="white" name="edit" size="20px" @click="showPhotoUpload('profile')"></q-icon>Edit your background image</div>
-        <div class="profile-photo text-center" @click="showPhotoUpload('profile')">
-            <div class="default-user-image column items-center" v-if="showDefaultPhoto()">
-                <q-avatar class="q-mb-sm" round="round" color="blue-grey-10" icon="person" font-size="110px" size="180px" text-color="white"></q-avatar><span class="text-caption text-blue-grey-10">Click to edit</span></div>
-            <div class="user-image column items-center" v-else>
-                <q-avatar class="q-mb-sm shadow-5" size="180px" @click="showPhotoUpload('profile')">
-                    <q-img :src="currentUser.profilePhoto"></q-img>
-                </q-avatar><span class="text-blue-grey-10"><q-icon class="q-mr-sm" color="blue-grey-10" name="edit" size="16px"></q-icon>Click to edit</span></div>
+        <div class="user-background" v-else>
+          <q-img :src="currentUser.backgroundPhoto" style="height: 200px;" @click="showPhotoUpload('background')"></q-img>
         </div>
-        <section class="user-info">
-            <h6 class="q-mt-none q-mb-md text-center">Edit Your Profile</h6>
-            <div class="row justify-between items-center q-mb-lg"><label class="col-3" for="fullName">Name</label>
-                <q-input class="col" id="fullName" v-model="fullName" borderless="borderless" dense="dense" type="text"></q-input>
-            </div>
-            <div class="row justify-between items-center q-mb-lg"><label class="col-3" for="email">Email</label>
-                <q-input class="col" id="email" v-model="email" borderless="borderless" dense="dense" type="text"></q-input>
-            </div>
-            <div class="row justify-between items-center q-mb-lg"><label class="col-3" for="mobile">Mobile</label>
-                <q-input class="col" id="mobile" v-model="mobile" borderless="borderless" dense="dense" hint="+1(###) ###-####" mask="+#(###) ###-####" type="text"></q-input>
-            </div>
-        </section>
-        <div class="row justify-between q-my-lg q-px-md absolute-bottom">
-            <q-btn color="primary" label="CANCEL" style="min-width:6em;" @click="setEditUserDialog(false)"></q-btn>
-            <q-btn color="primary" type="submit" label="SAVE" style="min-width:6em;"></q-btn>
+      </div>
+      <div class="absolute-top q-mt-sm text-white text-center">
+        <q-icon class="q-mr-sm" color="white" name="edit" size="20px" @click="showPhotoUpload('background')"></q-icon>Edit your background image</div>
+      <div class="profile-photo text-center" @click="showPhotoUpload('profile')">
+        <div class="default-user-image column items-center" v-if="showDefaultPhoto()">
+          <q-avatar class="q-mb-sm" round="round" color="blue-grey-10" icon="person" font-size="110px" size="180px" text-color="white"></q-avatar><span class="text-caption text-blue-grey-10">Click to edit</span></div>
+        <div class="user-image column items-center" v-else>
+          <q-avatar class="q-mb-sm shadow-5" size="180px" @click="showPhotoUpload('profile')">
+            <q-img :src="currentUser.profilePhoto"></q-img>
+          </q-avatar><span class="text-blue-grey-10"><q-icon class="q-mr-sm" color="blue-grey-10" name="edit" size="16px"></q-icon>Click to edit</span></div>
+      </div>
+      <section class="user-info">
+        <h6 class="q-mt-none q-mb-md text-center">Edit Your Profile</h6>
+        <div class="row justify-between items-center q-mb-lg"><label class="col-3" for="fullName">Name</label>
+          <q-input class="col" id="fullName" v-model="fullName" borderless="borderless" dense="dense" type="text"></q-input>
         </div>
+        <div class="row justify-between items-center q-mb-lg"><label class="col-3" for="email">Email</label>
+          <q-input class="col" id="email" v-model="email" borderless="borderless" dense="dense" type="text"></q-input>
+        </div>
+        <div class="row justify-between items-center q-mb-lg"><label class="col-3" for="mobile">Mobile</label>
+          <q-input class="col" id="mobile" v-model="mobile" borderless="borderless" dense="dense" hint="+1(###) ###-####" mask="+#(###) ###-####" type="text"></q-input>
+        </div>
+      </section>
+      <div class="row justify-between q-my-lg q-px-md absolute-bottom">
+        <q-btn color="primary" label="CANCEL" style="min-width:6em;" @click="setEditUserDialog(false)"></q-btn>
+        <q-btn color="primary" type="submit" label="SAVE" style="min-width:6em;"></q-btn>
+      </div>
     </q-form>
     <q-dialog v-model="photoUpload" transition-hide="scale" transition-show="scale" @before-hide="resetPhotoType">
-        <fbq-uploader
-          class="q-my-lg"
-          label="Please Upload a Photo"
-          :meta="meta"
-          :prefixPath="prefixPath"
-          @uploaded="uploadComplete"
-        ></fbq-uploader>
+      <fbq-uploader
+        class="q-my-lg"
+        label="Please Upload a Photo"
+        :meta="meta"
+        :prefixPath="prefixPath"
+        @uploaded="uploadComplete"
+      ></fbq-uploader>
     </q-dialog>
-</div>
+  </div>
 </template>
 
 <script>
-import { mapActions, mapMutations } from 'vuex'
-import { QUploaderBase } from 'quasar'
-export default {
-  name: 'UserSettings',
-  mixins: [ QUploaderBase ],
-  components: {
-    'fbq-uploader': () => import('../../../components/FBQUploader.vue')
-  },
-  data () {
-    const state = this.$store.state.user.currentUser
-    return {
-      email: state.email,
-      fullName: state.fullName,
-      mobile: state.mobile,
-      photoType: '',
-      photoUpload: false
-    }
-  },
-  computed: {
-    currentUser () {
-      return this.$store.state.user.currentUser
+  import { mapActions, mapMutations } from 'vuex'
+  import { QUploaderBase } from 'quasar'
+  export default {
+    name: 'UserSettings',
+    mixins: [ QUploaderBase ],
+    components: {
+      'fbq-uploader': () => import('../../../components/FBQUploader.vue')
     },
-    meta () {
+    data () {
+      const state = this.$store.state.user.currentUser
       return {
-        id: this.currentUser.id,
-        photoType: this.photoType
+        email: state.email,
+        fullName: state.fullName,
+        mobile: state.mobile,
+        photoType: '',
+        photoUpload: false
       }
     },
-    prefixPath () {
-      const id = this.currentUser.id,
-        path = `${id}/${this.photoType}Photo/${this.photoType}Photo.`
-      return path
-    }
-  },
-  methods: {
-    ...mapActions('user', ['updateUserData']),
-    ...mapMutations('user', ['setEditUserDialog']),
-    resetPhotoType () {
-      this.photoType = ''
+    computed: {
+      currentUser () {
+        return this.$store.state.user.currentUser
+      },
+      meta () {
+        return {
+          id: this.currentUser.id,
+          photoType: this.photoType
+        }
+      },
+      prefixPath () {
+        const id = this.currentUser.id,
+          path = `${id}/${this.photoType}Photo/${this.photoType}Photo.`
+        return path
+      }
     },
-    async saveUserData () {
-      const { currentUser, email, fullName, mobile } = this
+    methods: {
+      ...mapActions('user', ['updateUserData']),
+      ...mapMutations('user', ['setEditUserDialog']),
+      resetPhotoType () {
+        this.photoType = ''
+      },
+      async saveUserData () {
+        const { currentUser, email, fullName, mobile } = this
 
-      this.$q.loading.show({
-        message: 'Updating your data, please stand by...',
-        customClass: 'text-h3, text-bold'
-      })
-
-      try {
-        await this.updateUserData({
-          id: currentUser.id,
-          email,
-          fullName,
-          mobile
+        this.$q.loading.show({
+          message: 'Updating your data, please stand by...',
+          customClass: 'text-h3, text-bold'
         })
-      } catch (err) {
+
+        try {
+          await this.updateUserData({
+            id: currentUser.id,
+            email,
+            fullName,
+            mobile
+          })
+        } catch (err) {
+          this.$q.notify({
+            message: `Looks like a problem updating your profile: ${err}`,
+            color: 'negative'
+          })
+        } finally {
+          this.$q.loading.hide()
+          this.setEditUserDialog(false)
+        }
+      },
+      showBackgroundPhoto () {
+        return this.currentUser.backgroundPhoto === '' ||
+          this.currentUser.backgroundPhoto === null ||
+          this.currentUser.backgroundPhoto === undefined
+      },
+      showDefaultPhoto () {
+        return this.currentUser.profilePhoto === '' ||
+          this.currentUser.profilePhoto === null ||
+          this.currentUser.profilePhoto === undefined
+      },
+      showPhotoUpload (type) {
+        this.photoUpload = true
+        this.photoType = type
+      },
+
+      uploadComplete (info) {
+        let fileNames = []
+        info.files.forEach(file => fileNames.push(file))
+        this.photoUpload = false
         this.$q.notify({
-          message: `Looks like a problem updating your profile: ${err}`,
-          color: 'negative'
+          message: `Successfully uploaded your photo: ${fileNames}`,
+          color: 'positive'
         })
-      } finally {
-        this.$q.loading.hide()
-        this.setEditUserDialog(false)
       }
-    },
-    showBackgroundPhoto () {
-      return this.currentUser.backgroundPhoto === '' ||
-        this.currentUser.backgroundPhoto === null ||
-        this.currentUser.backgroundPhoto === undefined
-    },
-    showDefaultPhoto () {
-      return this.currentUser.profilePhoto === '' ||
-        this.currentUser.profilePhoto === null ||
-        this.currentUser.profilePhoto === undefined
-    },
-    showPhotoUpload (type) {
-      this.photoUpload = true
-      this.photoType = type
-    },
-
-    uploadComplete (info) {
-      let fileNames = []
-      info.files.forEach(file => fileNames.push(file))
-      this.photoUpload = false
-      this.$q.notify({
-        message: `Successfully uploaded your photo: ${fileNames}`,
-        color: 'positive'
-      })
     }
   }
-}
 </script>
 <style lang="stylus">
   .user-settings
