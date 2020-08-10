5005211177fc8127b2f11aa2a8ac312bb4837c87
Adam Purdy <adamp@kpapro.com>
2020-08-10 13:34:00 -0400

    chore(cli, ui): Update app cli v2 - Profile

--- data/profile/src/router/routes.js
+++ data/profile/src/router/routes.js
@@ -47,11 +47,9 @@ const routes = [
 ]
 
 // Always leave this as last one
-if (process.env.MODE !== 'ssr') {
-  routes.push({
-    path: '*',
-    component: () => import('pages/Error404.vue')
-  })
-}
+routes.push({
+  path: '*',
+  component: () => import('pages/Error404.vue')
+})
 
 export default routes
