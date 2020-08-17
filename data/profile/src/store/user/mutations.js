12ad2af4f6c5c6a70707498e7fc2dc758614b8ac
Adam Purdy <adamp@kpapro.com>
2020-08-17 10:18:56 -0600

    fix(firebase): Issues #19 & #20
    - Correct appropriate doc reference
    - Remove unneeded mutation

--- data/profile/src/store/user/mutations.js
+++ data/profile/src/store/user/mutations.js
@@ -1,7 +1,3 @@
-export const setCurrentUserData = (state, data) => {
-  state.currentUser = data
-}
-
 export const setEditUserDialog = (state, editUserDialog) => {
   state.editUserDialog = editUserDialog
 }
